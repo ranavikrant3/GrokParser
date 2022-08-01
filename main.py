cfe867005baedb29393192525c7351b42461cb46
vikrantrana1996 <vikrantrana@protonmail.com>
2022-08-01 09:47:23 -0400

    added todo.txt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygrok import Grok
-text = 'my name is vikrant and age is 26'
-pattern = 'my name is %{WORD:name} and age is %{NUMBER:age}'
+text = 'my name is vikrant and age is 26, my name is abc'
+pattern = 'my name is %{WORD:name} and age is %{NUMBER:age}, my name is %{WORD:secondname}'
 grok = Grok(pattern)
 print(grok.match(text))
