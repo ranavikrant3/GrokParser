75b32fad3b0c6c3df9b9cb216ae7736dc891dae1
vikrantrana1996 <vikrantrana@protonmail.com>
2022-08-01 09:32:49 -0400

    Added a basic grok parser

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from pygrok import Grok
+text = 'my name is vikrant and age is 26'
+pattern = 'my name is %{WORD:name} and age is %{NUMBER:age}'
+grok = Grok(pattern)
+print(grok.match(text))
